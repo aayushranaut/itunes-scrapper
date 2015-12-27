839a32ce003a7cb0b52bcfba7c759a0a567eb6ad
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-27 10:01:41 +0530

    Generated class App

--- itunes.py
+++ itunes.py
@@ -11,3 +11,12 @@ class AppCrawler:
 
     def get_app_from_link(self, link):
         return
+
+
+class App:
+
+    def __init__(self, name, developer, price, links)
+        self.name = name
+        self.developer = developer
+        self.price = price
+        self.links = links
