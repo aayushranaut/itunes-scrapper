3fb079e229ec2c4bd47a7e740c80a1ac13390fa8
Aayush Ranaut <aayush.ranaut@gmail.com>
2015-12-27 10:06:28 +0530

    created crawler method

--- itunes.py
+++ itunes.py
@@ -5,6 +5,7 @@ class AppCrawler:
     def __init__(self, starting_url, depth):
         self.starting_url = starting_url
         self.depth = depth
+        self.apps = []
 
     def crawl(self):
         return
@@ -24,4 +25,10 @@ class App:
     def __str__(self):
         return ("Name: " + self.name.encode('UTF-8') +
         "\r\nDeveloper: " + self.developer.encode('UTF-8') +
-        "\r\nPrice: " + self.price.encode('UTF-8') + "\r\n");
+        "\r\nPrice: " + self.price.encode('UTF-8') + "\r\n")
+
+crawler = AppCrawler("https://itunes.apple.com/us/app/candy-crush-saga/id553834731", 0)
+crawler.crawl()
+
+for app in self.apps:
+    print app
